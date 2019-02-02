fadabd8b35c81dd595bccd9f82f94461d4ce550f
ajayk111 <ajayk111@users.noreply.github.com>
2019-02-01 20:20:32 -0800

    Image Updates, Motion Code

--- Extreme Sports/Assets/PlayerController.cs
+++ Extreme Sports/Assets/PlayerController.cs
@@ -8,16 +8,34 @@ public class PlayerController : MonoBehaviour
 
     public string team;
 
+    private float forwardMove = 0f;
+    private float rotationMove = 0f;
+    public float speed = 1.0f;
+    public float angularSpeed = 1.0f;
+    
     private Transform transform;
+    private Vector2 lastVelocity;
+    private Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
         transform = GetComponent<Transform>();
+        rb2d = GetComponent<Rigidbody2D>();
+        lastVelocity = Vector2.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        forwardMove = Input.GetAxisRaw("Horizontal");
+        rotationMove = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 currentAcceleration = (rb2d.velocity - lastVelocity)/Time.deltaTime;
+        rb2d.velocity += new Vector2(forwardMove,0)*speed*Time.deltaTime;
+        rb2d.angularVelocity += rotationMove * angularSpeed * Time.deltaTime;
+
     }
 }
