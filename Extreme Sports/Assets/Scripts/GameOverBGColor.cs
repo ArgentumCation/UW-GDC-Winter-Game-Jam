1ee7a833207315664cd693b7e5b3ccbb366d33f0
ajayk111 <ajayk111@users.noreply.github.com>
2019-02-05 21:12:38 -0800

    Update GameOverBGColor.cs

--- Extreme Sports/Assets/Scripts/GameOverBGColor.cs
+++ Extreme Sports/Assets/Scripts/GameOverBGColor.cs
@@ -27,6 +27,9 @@ public class GameOverBGColor : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 }
